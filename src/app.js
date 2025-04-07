d55074948dc9365f65e6e0b9dc805dbabcf967f8
Fernando Elotlan <zS20020852@estudiantes.uv.mx>
2025-04-07 17:01:56 -0600

    Format JSON response with indentation for better readability

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import characterRoutes from './routes/CharactersRoutes.js';
 
 const app = express();
 
+app.set('json spaces', 2);
 app.use(express.json());
 app.use('/characters', characterRoutes);
 
